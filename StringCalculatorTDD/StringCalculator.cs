adcb0b25c26640c363f09453255fd91c9a67de9e
Richard Chalk <barmybrit@gmail.com>
2025-06-09 07:54:44 +0200

    Added comment about "out"

--- StringCalculatorTDD/StringCalculator.cs
+++ StringCalculatorTDD/StringCalculator.cs
@@ -16,6 +16,14 @@ namespace StringCalculatorTDD
             foreach (string part in parts)
             {
                 int number;
+
+                // Försök att omvandla texten (part) till ett heltal och
+                // spara resultatet i variabeln 'number'
+                // 'out' används för att skicka tillbaka ett extra
+                // värde från en metod.
+                // Utan 'out' kan du inte få ut själva siffran –
+                // bara om det lyckades eller inte.
+                // Det är alltså ett sätt att returnera två saker på en gång:
                 bool isNumber = int.TryParse(part, out number);
                 if (isNumber && number >= 0 && number <=1000)
                     sum += number;
